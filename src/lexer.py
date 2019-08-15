ee4f62df63dcd6331e4977774285cf76cc1acb0a
Jorge Rossi <jorgefzrossi@gmail.com>
2019-08-14 21:40:18 -0300

    Testes unitários lexer

--- src/lexer.py
+++ src/lexer.py
@@ -1,101 +1,97 @@
 import ply.lex as lex
 
 reserved = {
-    'se': 'IF',
-    'então': 'THEN',
-    'senão': 'ELSE',
-    'fim': 'END',
-    'repita': 'REPEAT',
-    'até': 'UNTIL',
-    'leia': 'READ',
-    'escreva': 'WRITE',
-    'retorna': 'RETURN',
-    'inteiro': 'INTEGER',
-    'flutuante': 'FLOAT'
+    'se': 'SE',
+    'então': 'ENTAO',
+    'senão': 'SENAO',
+    'fim': 'FIM',
+    'repita': 'REPITA',
+    'até': 'ATE',
+    'leia': 'LEIA',
+    'escreva': 'ESCREVA',
+    'retorna': 'RETORNA',
+    'flutuante': 'FLUTUANTE',
+    'inteiro': 'INTEIRO'
 }
 
 tokens = [
-    'COLLON',
-    'SEMICOLLON',
-    'COMMA',
+    'IDENTIFICADOR',
+    'FLUTUANTE_LITERAL',
+    'INTEIRO_LITERAL',
 
-    'ASSIGN',
+    'DOIS_PONTOS',
+    'VIRGULA',
+    'ATRIBUICAO',
 
-    'LESSER',
-    'GREATER',
-    'LESSER_EQUALS',
-    'GREATER_EQUALS',
-    'EQUALS',
-    'NOT_EQUALS',
+    'IGUAL',
+    'DIFERENTE',
+    'MENOR',
+    'MENOR_IGUAL',
+    'MAIOR',
+    'MAIOR_IGUAL',
 
-    'PLUS',
-    'MINUS',
-    'TIMES',
-    'DIVISION',
+    'E',
+    'OU',
+    'NAO',
 
-    'AND',
-    'OR',
-    'NOT',
+    'ADICAO',
+    'SUBTRACAO',
+    'MULTIPLICACAO',
+    'DIVISAO',
 
-    'ID',
+    'ABRE_PARENTESES',
+    'FECHA_PARENTESES',
+    'ABRE_COLCHETES',
+    'FECHA_COLCHETES',
 
-    'FLOAT_LITERAL',
-    'INTEGER_LITERAL',
-
-    'SQUARE_BRACKET_L',
-    'SQUARE_BRACKET_R',
-    'PARENTESIS_L',
-    'PARENTESIS_R',
-
-    'COMMENT'
+    'COMENTARIO'
 ] + list(reserved.values())
 
 
-def t_COMMENT(t):
+def t_COMENTARIO(t):
     r'\{[^\}]*[^\{]*\}'
     contador = t.value.count("\n")
     t.lexer.lineno += contador
 
 
-def t_FLOAT_LITERAL(t):
+def t_IDENTIFICADOR(t):
+    r'[a-zA-Z_záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ][a-zA-Z_0-9záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ]*'
+    t.type = reserved.get(t.value, 'IDENTIFICADOR')
+    return t
+
+
+def t_FLUTUANTE_LITERAL(t):
     r'^ [+-]?\d+(?: \.\d*(?: [eE][+-]?\d+)?)?$'
     t.value = float(t.value)
     return t
 
 
-def t_INTEGER_LITERAL(t):
+def t_INTEIRO_LITERAL(t):
     r'\d+'
     t.value = int(t.value)
     return t
 
 
-def t_ID(t):
-    r'[a-zA-Z_][a-zA-Z_0-9]*'
-    t.type = reserved.get(t.value, 'ID')
-    return t
-
-
-t_COLLON = r':'
-t_SEMICOLLON = r';'
-t_COMMA = r','
-t_ASSIGN = r':='
-t_LESSER = r'<'
-t_GREATER = r'>'
-t_LESSER_EQUALS = r'<='
-t_GREATER_EQUALS = r'>='
-t_EQUALS = r'='
-t_NOT_EQUALS = r'<>'
-t_AND = r'\&\&'
-t_OR = r'\|\|'
-t_NOT = r'!'
-t_PLUS = r'\+'
-t_MINUS = r'\-'
-t_TIMES = r'\*'
-t_DIVISION = r'\/'
-t_SQUARE_BRACKET_L = r'\['
-t_SQUARE_BRACKET_R = r'\]'
-t_PARENTESIS_L = r'\('
-t_PARENTESIS_R = r'\)'
+t_DOIS_PONTOS = r':'
+t_VIRGULA = r','
+t_ATRIBUICAO = r':='
+t_IGUAL = r'='
+t_DIFERENTE = r'<>'
+t_MENOR = r'<'
+t_MENOR_IGUAL = r'<='
+t_MAIOR = r'>'
+t_MAIOR_IGUAL = r'>='
+t_E = r'\&\&'
+t_OU = r'\|\|'
+t_NAO = r'!'
+t_ADICAO = r'\+'
+t_SUBTRACAO = r'\-'
+t_MULTIPLICACAO = r'\*'
+t_DIVISAO = r'\/'
+t_ABRE_PARENTESES = r'\('
+t_FECHA_PARENTESES = r'\)'
+t_ABRE_COLCHETES = r'\['
+t_FECHA_COLCHETES = r'\]'
 
 
 t_ignore = ' \t'
@@ -107,16 +103,8 @@ def t_newline(t):
 
 
 def t_error(t):
-    print("Illegal character '%s'" % t.value[0])
+    print("Caractere inválido: '%s'" % t.value[0])
     t.lexer.skip(1)
 
 
 lexer = lex.lex()
-
-arq = open('./tests/lexer_001.tpp')
-lexer.input(arq.read())
-arq.close()
-tokens = [t for t in lexer]
-
-for tok in tokens:
-    print(tok)
---
+++ src/lexer_test.py
@@ -0,0 +1,52 @@
+import unittest
+from lexer import lexer
+
+
+class TestLexer(unittest.TestCase):
+    def test_fatorial(self):
+
+        arq = open('./codes/fatorial.tpp')
+        lexer.input(arq.read())
+        arq.close()
+
+        tokens = list(map(lambda token: (token.type, token.value),
+                          [token for token in lexer]))
+
+        for t in tokens:
+            print(t)
+
+        self.assertSequenceEqual(
+            tokens, [('INTEIRO', 'inteiro'), ('DOIS_PONTOS', ':'), ('IDENTIFICADOR', 'n'),
+                     ('INTEIRO', 'inteiro'), ('IDENTIFICADOR', 'fatorial'), ('ABRE_PARENTESES',
+                                                                             '('), ('INTEIRO', 'inteiro'), ('DOIS_PONTOS', ':'), ('IDENTIFICADOR', 'n'), ('FECHA_PARENTESES', ')'),
+                     ('INTEIRO', 'inteiro'), ('DOIS_PONTOS',
+                                              ':'), ('IDENTIFICADOR', 'fat'),
+                     ('SE', 'se'), ('IDENTIFICADOR', 'n'), ('MAIOR',
+                                                            '>'), ('INTEIRO_LITERAL', 0), ('ENTAO', 'então'),
+                     ('IDENTIFICADOR', 'fat'), ('ATRIBUICAO',
+                                                ':='), ('INTEIRO_LITERAL', 1),
+                     ('REPITA', 'repita'),
+                     ('IDENTIFICADOR', 'fat'), ('ATRIBUICAO', ':='), ('IDENTIFICADOR',
+                                                                      'fat'), ('MULTIPLICACAO', '*'), ('IDENTIFICADOR', 'n'),
+                     ('IDENTIFICADOR', 'n'), ('ATRIBUICAO', ':='), ('IDENTIFICADOR',
+                                                                    'n'), ('SUBTRACAO', '-'), ('INTEIRO_LITERAL', 1),
+                     ('ATE', 'até'), ('IDENTIFICADOR',
+                                      'n'),  ('IGUAL', '='), ('INTEIRO_LITERAL', 0),
+                     ('RETORNA', 'retorna'), ('ABRE_PARENTESES', '('), ('IDENTIFICADOR',
+                                                                        'fat'), ('FECHA_PARENTESES', ')'), ('SENAO', 'senão'),
+                     ('RETORNA', 'retorna'), ('ABRE_PARENTESES',
+                                              '('),  ('INTEIRO_LITERAL', 0), ('FECHA_PARENTESES', ')'), ('FIM', 'fim'), ('FIM', 'fim'),
+                     ('INTEIRO', 'inteiro'), ('IDENTIFICADOR',
+                                              'principal'), ('ABRE_PARENTESES', '('), ('FECHA_PARENTESES', ')'),
+                     ('LEIA', 'leia'), ('ABRE_PARENTESES',
+                                        '('), ('IDENTIFICADOR', 'n'), ('FECHA_PARENTESES', ')'),
+                     ('ESCREVA', 'escreva'), ('ABRE_PARENTESES', '('), ('IDENTIFICADOR', 'fatorial'), ('ABRE_PARENTESES',
+                                                                                                       '('), ('IDENTIFICADOR', 'n'), ('FECHA_PARENTESES', ')'), ('FECHA_PARENTESES', ')'),
+                     ('RETORNA', 'retorna'), ('ABRE_PARENTESES',
+                                              '('), ('INTEIRO_LITERAL', 0), ('FECHA_PARENTESES', ')'),
+                     ('FIM', 'fim')
+                     ])
+
+
+if __name__ == '__main__':
+    unittest.main()
