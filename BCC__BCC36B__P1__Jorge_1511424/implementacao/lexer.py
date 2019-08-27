01fef6de033b02e23d45b4e5819cd7dc54647960
Jorge Franzon <jorgefzrossi@gmail.com>
2019-08-27 15:04:06 -0300

    Corrigindo lexer

--- BCC__BCC36B__P1__Jorge_1511424/implementacao/lexer.py
+++ BCC__BCC36B__P1__Jorge_1511424/implementacao/lexer.py
@@ -1,110 +1,127 @@
 import ply.lex as lex
+import colorama
 
-reserved = {
-    'se': 'SE',
-    'então': 'ENTAO',
-    'senão': 'SENAO',
-    'fim': 'FIM',
-    'repita': 'REPITA',
-    'até': 'ATE',
-    'leia': 'LEIA',
-    'escreva': 'ESCREVA',
-    'retorna': 'RETORNA',
-    'flutuante': 'FLUTUANTE',
-    'inteiro': 'INTEIRO'
-}
-
-tokens = [
-    'IDENTIFICADOR',
-    'FLUTUANTE_LITERAL',
-    'INTEIRO_LITERAL',
-
-    'DOIS_PONTOS',
-    'VIRGULA',
-    'ATRIBUICAO',
-
-    'IGUAL',
-    'DIFERENTE',
-    'MENOR',
-    'MENOR_IGUAL',
-    'MAIOR',
-    'MAIOR_IGUAL',
-
-    'E',
-    'OU',
-    'NAO',
-
-    'ADICAO',
-    'SUBTRACAO',
-    'MULTIPLICACAO',
-    'DIVISAO',
-
-    'ABRE_PARENTESES',
-    'FECHA_PARENTESES',
-    'ABRE_COLCHETES',
-    'FECHA_COLCHETES',
-
-    'COMENTARIO'
-] + list(reserved.values())
+colorama.init()
 
 
-def t_COMENTARIO(t):
-    r'{.*}'
-    contador = t.value.count("\n")
-    t.lexer.lineno += contador
+class TppLexer(object):
 
+    reserved = {
+        'se': 'SE',
+        'então': 'ENTAO',
+        'senão': 'SENAO',
+        'repita': 'REPITA',
+        'até': 'ATE',
+        'fim': 'FIM',
+        'retorna': 'RETORNA',
+        'leia': 'LEIA',
+        'escreva': 'ESCREVA',
+        'flutuante': 'FLUTUANTE',
+        'inteiro': 'INTEIRO'
+    }
 
-def t_IDENTIFICADOR(t):
-    r'[a-zA-Z_záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ][a-zA-Z_0-9záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ]*'
-    t.type = reserved.get(t.value, 'IDENTIFICADOR')
-    return t
+    tokens = [
 
+        'SOMA',
+        'SUBTRACAO',
+        'MULTIPLICACAO',
+        'DIVISAO',
 
-def t_FLUTUANTE_LITERAL(t):
-    r'(\d+\.?\d*[eE]-?[1-9]\d*)|(\d+\.\d+)'
-    t.value = float(t.value)
-    return t
+        'IGUALDADE',
+        'DESIGUALDADE',
+        'ATRIBUICAO',
+        'MENOR',
+        'MENOR_IGUAL',
+        'MAIOR',
+        'MAIOR_IGUAL',
 
+        'VIRGULA',
+        'DOIS_PONTOS',
 
-def t_INTEIRO_LITERAL(t):
-    r'0|[1-9]\d*'
-    t.value = int(t.value)
-    return t
+        'ID',
+        'NUM_INTEIRO',
+        'NUM_PONTO_FLUTUANTE',
+        'NUM_NOTACAO_CIENTIFICA',
 
+        'E_LOGICO',
+        'OU_LOGICO',
+        'NEGACAO',
 
-t_DOIS_PONTOS = r':'
-t_VIRGULA = r','
-t_ATRIBUICAO = r':='
-t_IGUAL = r'='
-t_DIFERENTE = r'<>'
-t_MENOR = r'<'
-t_MENOR_IGUAL = r'<='
-t_MAIOR = r'>'
-t_MAIOR_IGUAL = r'>='
-t_E = r'\&\&'
-t_OU = r'\|\|'
-t_NAO = r'!'
-t_ADICAO = r'\+'
-t_SUBTRACAO = r'\-'
-t_MULTIPLICACAO = r'\*'
-t_DIVISAO = r'\/'
-t_ABRE_PARENTESES = r'\('
-t_FECHA_PARENTESES = r'\)'
-t_ABRE_COLCHETES = r'\['
-t_FECHA_COLCHETES = r'\]'
+        'ABRE_PAR',
+        'FECHA_PAR',
+        'ABRE_COL',
+        'FECHA_COL',
 
+        'COMENTARIO'
+    ] + list(reserved.values())
 
-t_ignore = ' \t'
+    t_DOIS_PONTOS = r':'
+    t_VIRGULA = r','
+    t_ATRIBUICAO = r':='
+    t_IGUALDADE = r'='
+    t_DESIGUALDADE = r'<>'
+    t_MENOR = r'<'
+    t_MENOR_IGUAL = r'<='
+    t_MAIOR = r'>'
+    t_MAIOR_IGUAL = r'>='
+    t_E_LOGICO = r'\&\&'
+    t_OU_LOGICO = r'\|\|'
+    t_NEGACAO = r'!'
+    t_SOMA = r'\+'
+    t_SUBTRACAO = r'\-'
+    t_MULTIPLICACAO = r'\*'
+    t_DIVISAO = r'\/'
+    t_ABRE_PAR = r'\('
+    t_FECHA_PAR = r'\)'
+    t_ABRE_COL = r'\['
+    t_FECHA_COL = r'\]'
 
+    t_ignore = ' \t'
 
-def t_newline(t):
-    r'\n+'
-    t.lexer.lineno += len(t.value)
+    def __init__(self, **kwargs):
+        self.lexer = lex.lex(module=self, **kwargs)
 
+    def t_COMENTARIO(self, t):
+        r'{.*}'
+        contador = t.value.count("\n")
+        t.lexer.lineno += contador
 
-def t_error(t):
-    print("Caractere inválido: '%s'" % t.value[0])
-    t.lexer.skip(1)
+    def t_ID(self, t):
+        r'[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ_][0-9A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ_]*'
+        t.type = self.reserved.get(t.value, 'ID')
+        return t
 
+    def t_NUM_PONTO_FLUTUANTE(self, t):
+        r'\d+\.\d+'
+        t.value = float(t.value)
+        return t
 
-lexer = lex.lex()
+    def t_NUM_NOTACAO_CIENTIFICA(self, t):
+        r'\d+(\.?)[eE](-)?\d+'
+        t.value = float(t.value)
+        return t
+
+    def t_NUM_INTEIRO(self, t):
+        r'0|[1-9]\d*'
+        t.value = int(t.value)
+        return t
+
+    def t_newline(self, t):
+        r'\n+'
+        t.lexer.lineno += len(t.value)
+
+    def t_error(self, t):
+        print("Caractere inválido: '%s'" % t.value[0])
+        t.lexer.skip(1)
+
+    def input_data(self, data):
+        self.lexer.input(data)
+
+    def print_tokens(self):
+        count = 0
+        for tok in self.lexer:
+            print(colorama.Fore.BLUE, '[', tok.type, ']',
+                  colorama.Style.RESET_ALL, ' ', tok.value, sep='')
+            count += 1
+        print('--------------')
+        print('Número de tokens: ', count)
--- BCC__BCC36B__P1__Jorge_1511424/implementacao/tpp.py
+++ BCC__BCC36B__P1__Jorge_1511424/implementacao/tpp.py
@@ -2,23 +2,38 @@
 
 import sys
 import os
-from parser2 import parser, Node
+import argparse
+import lexer
 
-# program_name = sys.argv[1]
-f = open(os.path.join(os.path.dirname(__file__), '../codes/test.tpp'))
-result = parser.parse(f.read())
-f.close()
+parser = argparse.ArgumentParser(description='Compilador da linguagem TPP')
+parser.add_argument('source', type=str, help='Arquivo de entrada')
+
+args = parser.parse_args()
+
+arq = open(args.source, 'r', encoding='utf-8')
+data = arq.read()
+arq.close()
+
+tppLexer = lexer.TppLexer()
+tppLexer.input_data(data)
+
+tppLexer.print_tokens()
+
+# # program_name = sys.argv[1]
+# f = open(os.path.join(os.path.dirname(__file__), '../codes/test.tpp'))
+# result = parser.parse(f.read())
+# f.close()
 
 
-def imprimir(node, i):
-    print(' ' * i, node)
+# def imprimir(node, i):
+#     print(' ' * i, node)
 
-    if node.value is not None:
-        if node.value is Node:
-            imprimir(node.value, i+1)
-        else:
-            for n in node.value:
-                imprimir(n, i+1)
+#     if node.value is not None:
+#         if node.value is Node:
+#             imprimir(node.value, i+1)
+#         else:
+#             for n in node.value:
+#                 imprimir(n, i+1)
 
 
-imprimir(result, 0)
+# imprimir(result, 0)
